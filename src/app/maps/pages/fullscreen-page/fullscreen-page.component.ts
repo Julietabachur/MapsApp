52239652bcb225609d05213aa8d6ce1ece9421bc
Julieta BACHUR <jbachur@CNP.com.ar>
2024-02-23 15:29:58 -0300

    listo

---
+++ src/app/maps/pages/fullscreen-page/fullscreen-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './fullscreen-page.component.html',
+  styleUrls: ['./fullscreen-page.component.css']
+})
+export class FullscreenPageComponent {
+
+}
